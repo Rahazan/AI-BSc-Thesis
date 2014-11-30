d67c2a46599236e2dfc57e4ead154538427173d4
Guido Zuidhof <me@guido.io>
2014-11-30 14:57:52 +0100

    Fix in timestamp

--- measurements/cutdata.bat
+++ measurements/cutdata.bat
@@ -1,4 +1,4 @@
-neko ../ResultCutter/bin/ResultCutter.n 13:15:01 1000 D:\Dropbox\bsc\Task\jasper.txt jasperdat.txt
+neko ../ResultCutter/bin/ResultCutter.n 14:14:38 1000 D:\Dropbox\bsc\Task\jasper.txt jasperdat.txt
 neko ../ResultCutter/bin/ResultCutter.n 20:57:36 1000 D:\Dropbox\bsc\Task\luc.txt lucdat.txt
 neko ../ResultCutter/bin/ResultCutter.n 18:52:22 1000 D:\Dropbox\bsc\Task\robbert.txt robbertdat.txt
 neko ../ResultCutter/bin/ResultCutter.n 01:18:04 1000 D:\Dropbox\bsc\Task\steven.txt stevendat.txt
